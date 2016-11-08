34d7daa529167f2ed5c94cb7a5839c429a380e6c
athulkrishnan <mail@athulkrishnan.com>
2016-11-08 07:01:16 +0530

    first commit

--- comicmachine/settings_loc.py
+++ comicmachine/settings_loc.py
@@ -11,15 +11,28 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd8absrlmrmhfhc',
+#         'USER': 'afajjrbqjhblnz',
+#         'PASSWORD': 'UzGS2IWFjQr0Iw_7B0oDVLa7Oq',
+#         'HOST': 'ec2-54-204-39-67.compute-1.amazonaws.com'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8absrlmrmhfhc',
-        'USER': 'afajjrbqjhblnz',
-        'PASSWORD': 'UzGS2IWFjQr0Iw_7B0oDVLa7Oq',
-        'HOST': 'ec2-54-204-39-67.compute-1.amazonaws.com'
+        'NAME': 'bobanandmolly',
+        'USER': 'athulkrishnan',
+        'PASSWORD': 'W@lkinglif3+',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
